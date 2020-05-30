d6f08cb5bf2197f9ec0b8d48a7041c91b67cc9d8
osmanakol <osmanakol@outlook.com>
2020-05-31 00:14:14 +0300

    create server.ts and listen to host:port

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { host,port  } from "../host.json";
 import app from "./app";
 
 app.listen(port,()=>{
-    console.log(`Server çalışıyor, http://${host}:${port}`);
+    console.log(`Server çalışıyor, http://${host}:${port}/api`);
 })
